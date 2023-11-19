df2e3338d6f0cb9bf45de6cf2f82f316a1a602ab
yashar <zavaryyashar009@gmail.com>
2023-11-19 18:54:22 +0330

    main button for adding new team to the game

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from tkinter import *
 
 
-
+def addnewTeam(event):
+    pass
 
 def mainPage():
+    """_summary_
+        this will show us main page that we can add update and show score board button in this part
+    """
     # main page root
     mainPageRoot = Tk()
     # main page title
     mainPageRoot.title('mainpage')
     # main page icon
     mainPageRoot.iconbitmap('icons\\mainpage.ico')
+    
+    # my background color
+    mainPageRoot.configure(bg='#F4CE14')
     
     # size of the main page
     w = 500
@@ -22,8 +29,13 @@ def mainPage():
     # set geometry of the main page
     mainPageRoot.geometry('%dx%d+%d+%d'%(w,h,x,y))
 
-
-
+    # our add new person button
+    addNewButton = Button(master= mainPageRoot, text='new team', bd='5', width=10, height=2, bg='#F5F7F8')
+    addNewButton.bind('<Button>', addnewTeam)
+    addNewButton.bind('<Enter>', lambda x: addNewButton.config(bg='#495E57'))
+    addNewButton.bind('<Leave>', lambda x: addNewButton.config(bg='#F5F7F8'))
+    addNewButton.pack()
+    
 
     # main loop of the main page
     mainPageRoot.mainloop()
